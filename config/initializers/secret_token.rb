106157cbb9e3a10a3780e8028be6c905c6680694
ban <banlv54@gmail.com>
2013-04-03 09:36:25 +0700

    review

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyApp::Application.config.secret_token = '7a67a39a1958b97182bcd1cb8cd6bcebc80abd8c5ed71873bef2abaf5600537a19dfceff6134d1484e52e64aa395255d749ba823e76ae0c19d0698a0426a9b27'
